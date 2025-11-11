ded6d5161a686d36a0acd3f4999306c70faf5f28
MaxP-Robotics <148493257+MP2008@users.noreply.github.com>
2025-11-11 15:48:14 -0500

    New flywheel speed test class for 2 motors.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/teleop/FlywheelSpeedTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/teleop/FlywheelSpeedTest.java
@@ -9,7 +9,9 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name = "FlywheelSpeedTest")
 public class FlywheelSpeedTest extends LinearOpMode {
 
-    private static final String FLYWHEEL_NAME = "flywheel";
+    private static final String M1name = "flywheel1";
+    private static final String M2name = "flywheel2";
+
 
     private static final double TICKS_PER_REV = 537.6;
 
@@ -25,11 +27,15 @@ public class FlywheelSpeedTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotorEx flywheel = hardwareMap.get(DcMotorEx.class, FLYWHEEL_NAME);
+        DcMotorEx m1 = hardwareMap.get(DcMotorEx.class, M1name);
+        DcMotorEx m2 = hardwareMap.get(DcMotorEx.class, M2name);
 
-        flywheel.setDirection(DcMotorSimple.Direction.FORWARD);
-        flywheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        m1.setDirection(DcMotorSimple.Direction.FORWARD);
+        m2.setDirection(DcMotorSimple.Direction.REVERSE);
+        m1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        m1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        m2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry.addLine("FlywheelSpeedTest ready.");
         telemetry.addLine("Right stick: up = faster, down = slower.");
@@ -37,40 +43,47 @@ public class FlywheelSpeedTest extends LinearOpMode {
 
         waitForStart();
 
-        double targetPower = 0.0;
-
         while (opModeIsActive()) {
             double stick = -gamepad1.right_stick_y;
 
-            targetPower += stick * POWER_STEP;
-            targetPower = clamp01(targetPower);
+            double power = stick;
+            if (stick > 0) {
+                power += 0.02 * stick;  // increase power gradually
+            } else if (stick < 0) {
+                power += 0.02 * stick;  // decrease power gradually
+            }
 
-            flywheel.setPower(targetPower);
+            m1.setPower(power);
+            m2.setPower(power);
 
-            double ticksPerSec = Math.abs(flywheel.getVelocity());
+            double tps1 = Math.abs(m1.getVelocity());
+            double tps2 = Math.abs(m2.getVelocity());
 
-            double revsPerSec = ticksPerSec / TICKS_PER_REV;                  // output-shaft rev/s
-            double rpm        = revsPerSec * SECONDS_PER_MINUTE;
+            double rpm1 = (tps1 / TICKS_PER_REV) * SECONDS_PER_MINUTE;
+            double rpm2 = (tps2 / TICKS_PER_REV) * SECONDS_PER_MINUTE;
 
-            double inchesPerSec = revsPerSec * FLYWHEEL_CIRCUMFERENCE_IN;
-            double feetPerSec   = inchesPerSec / INCHES_PER_FOOT;
-            double metersPerSec = inchesPerSec / INCHES_PER_METER;
+            double ips1 = (rpm1 / SECONDS_PER_MINUTE) * FLYWHEEL_CIRCUMFERENCE_IN;
+            double ips2 = (rpm2 / SECONDS_PER_MINUTE) * FLYWHEEL_CIRCUMFERENCE_IN;
 
-            telemetry.addData("Stick", "%.2f", stick);
-            telemetry.addData("Power", "%.2f", targetPower);
-            telemetry.addData("Ticks/sec", "%.1f", ticksPerSec);
-            telemetry.addData("RPM", "%.1f", rpm);
-            telemetry.addData("Rim Speed", "%.2f in/s  |  %.2f ft/s  |  %.2f m/s",
-                    inchesPerSec, feetPerSec, metersPerSec);
+            telemetry.addData("Stick (up=+)", "%.2f", stick);
+            telemetry.addData("Power", "%.2f", power);
+
+            telemetry.addLine("--- Motor 1 ---");
+            telemetry.addData("Ticks/sec", "%.1f", tps1);
+            telemetry.addData("RPM", "%.1f", rpm1);
+            telemetry.addData("Rim Speed", "%.2f in/s | %.2f ft/s | %.2f m/s",
+                    ips1, ips1 / INCHES_PER_FOOT, ips1 / INCHES_PER_METER);
+
+            telemetry.addLine("--- Motor 2 ---");
+            telemetry.addData("Ticks/sec", "%.1f", tps2);
+            telemetry.addData("RPM", "%.1f", rpm2);
+            telemetry.addData("Rim Speed", "%.2f in/s | %.2f ft/s | %.2f m/s",
+                    ips2, ips2 / INCHES_PER_FOOT, ips2 / INCHES_PER_METER);
+
             telemetry.update();
         }
 
-        flywheel.setPower(0.0);
-    }
-
-    private static double clamp01(double v) {
-        if (v < 0.0) return 0.0;
-        if (v > 1.0) return 1.0;
-        return v;
+        m1.setPower(0.0);
+        m2.setPower(0.0);
     }
 }
