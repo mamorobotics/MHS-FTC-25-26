b0a44dd4d17e055fdecbd2eb1f576ec8e21cc4f6
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2026-01-14 20:11:46 -0500

    added gate tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/teleop/TestDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/teleop/TestDriving.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.ours.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "testDriving")
 public class TestDriving extends OpMode {
@@ -11,12 +12,23 @@ public class TestDriving extends OpMode {
 
     private double motorPower = 0.2;
 
+    private Servo gate;
+
+    private double gatepos;
+    private double gateOpenPos = 0.01;
+    private double gateClosePos = 0.02;
+
+    private boolean gateDown = true;
+    private double gateStep = 0.005;
+
     @Override
     public void init() {
         driveTrain.setDriveTrain(hardwareMap, "frontLeft", "backLeft", "frontRight", "backRight");
         driveTrain.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         driveTrain.setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
         driveTrain.reverse(DriveTrain.DriveTrainSide.LEFT);
+
+        gate = hardwareMap.get(Servo.class, "gate");
     }
 
 
@@ -34,5 +46,33 @@ public class TestDriving extends OpMode {
             DriveTrain.FR.setPower(0);
             DriveTrain.BR.setPower(0);
         }
+
+        if (gamepad2.aWasPressed()) {
+            gatepos = gateOpenPos;
+            gateDown = false;
+        }
+        if (gamepad2.bWasPressed()) {
+            gatepos = gateClosePos;
+            gateDown = true;
+        }
+
+        if (gamepad2.dpadUpWasPressed()) {
+            gatepos += gateStep;
+        }
+        else if (gamepad2.dpadDownWasPressed()) {
+            gatepos -= gateStep;
+        }
+
+        if (!gateDown) {
+            gateOpenPos = gatepos;
+        }
+        if (gateDown) {
+            gateClosePos = gatepos;
+        }
+
+        telemetry.addData("Gate Closed Pos:", gateClosePos);
+        telemetry.addData("Gate Open Pos:", gateOpenPos);
+
+        gate.setPosition(gatepos);
     }
 }
