2ccd68127868b364a1bc77c62970eee99c37e310
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2026-01-19 20:15:50 -0500

    moved flywheelspeedtest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/customtuning/FlywheelSpeedTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/customtuning/FlywheelSpeedTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.ours.teleop;
+package org.firstinspires.ftc.teamcode.ours.customtuning;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
